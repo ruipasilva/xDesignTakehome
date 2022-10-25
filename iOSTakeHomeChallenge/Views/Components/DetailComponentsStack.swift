e5d8ecbf98f5f0669fdb6f8516c98233038de669
ruipasilva <ruipasilva@gmail.com>
2022-10-26 00:02:00 +0100

    Clean Up

---
+++ iOSTakeHomeChallenge/Views/Components/DetailComponentsStack.swift
@@ -0,0 +1,20 @@
+//
+//  DetailComponentsStack.swift
+//  iOSTakeHomeChallenge
+//
+//  Created by Rui on 25/10/2022.
+//
+
+import SwiftUI
+
+struct DetailComponentsStack: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailComponentsStack_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailComponentsStack()
+    }
+}
